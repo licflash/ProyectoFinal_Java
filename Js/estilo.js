c07e39d63d23ff3051b516fe5ee1d88eff264047
licflash <santangelo.lucia94@gmail.com>
2023-08-15 12:32:40 -0300

    vol_2

---
+++ Js/estilo.js
@@ -0,0 +1,10 @@
+//Defino las variables
+
+const cuerpo = document.body
+const titulo = document.getelementByClassName('titulo')
+const juego = document.getelementByClassName('juego')
+const moneda = document.getElementByClassName('moneda')
+const impuesto = document.getElementByClassName('porcentajeimpuesto')
+
+
+cuerpo.style.backgroundColor = 'black'
