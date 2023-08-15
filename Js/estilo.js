7c9e435e7574c9a2118c5ccb0f47c6d1c48def50
licflash <santangelo.lucia94@gmail.com>
2023-08-15 14:47:46 -0300

    vol_3

--- Js/estilo.js
+++ Js/estilo.js
@@ -1,10 +1,28 @@
-//Defino las variables
+// Defino las variables
+const cuerpo = document.body;
+const titulo = document.getElementsByClassName('titulo')[0];
+const juego = document.getElementsByClassName('juego')[0];
+const moneda = document.getElementsByClassName('moneda')[0];
+const impuesto = document.getElementsByClassName('porcentajeimpuesto')[0];
+const nuevojuegoboton = document.getElementById('nuevojuego');
 
-const cuerpo = document.body
-const titulo = document.getelementByClassName('titulo')
-const juego = document.getelementByClassName('juego')
-const moneda = document.getElementByClassName('moneda')
-const impuesto = document.getElementByClassName('porcentajeimpuesto')
+cuerpo.style.backgroundColor = 'black';
+cuerpo.style.color = 'white';
+document.body.style.textAlign = 'center';
 
+const contenedores = document.querySelectorAll('.formulario, .section-title, .juego, .moneda, .porcentajeimpuesto, .text-center');
 
-cuerpo.style.backgroundColor = 'black'
+for (const contenedor of contenedores) {
+    contenedor.style.marginBottom = '20px'; // Ajusta el valor según tu preferencia
+}
+
+// Selecciona todos los elementos con clase 'text-center' y 'text-center2'
+const buttonContainers = document.querySelectorAll('.text-center, .text-center2');
+
+// Aplica estilos a cada contenedor seleccionado
+for (const buttonContainers of buttonContainers) {
+    container.style.display = 'flex';
+    container.style.justifyContent = 'space-between';
+    container.style.alignItems = 'center'; // Alinea los elementos verticalmente en el centro
+    container.style.marginTop = '20px'; // Agregar margen arriba
+}
